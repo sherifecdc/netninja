9801fc927c4572467d8b08abf1e1b29e7400fb25
Sherif <sherif.ecdc@gmail.com>
2017-02-22 19:49:35 -0500

    tut25:: firebase intro: reading from db

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -7,7 +7,7 @@ export class DataService {
   constructor(private http: Http) { }
 
   fetchData(){
-    return this.http.get('assets/ninjas.json')
+    return this.http.get('https://sherif-ninja.firebaseio.com/.json')
     // .subscribe(
     // (data) => console.log(data)
     // )
